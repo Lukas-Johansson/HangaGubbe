bb320d253189f24e0862099f05e7aa00056b1746
Lukas-Johansson <lukas.johansson4@elev.ga.ntig.se>
2022-02-01 14:56:52 +0100

    Mer saker

--- src/FurkaDurka.java
+++ src/FurkaDurka.java
@@ -7,12 +7,21 @@ import java.util.Scanner;
 public class FurkaDurka {
     public static void main(String[] args) {
 
-        System.out.println("Please type in your own word");
-        String phrase = input.nextLine();
-        phrase = phrase.trim();
-        phrase = phrase.replaceAll(" ", "  ");
-        phrase = phrase.toUpperCase();
-        String phrase1 = phrase.replaceAll("[A-Z]", "_ ");
+        System.out.println("Please type in your word");
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.nextLine();
+        String[] bert = word.split("");
+
+        for (int potatis2 = 0; potatis2 <= bert.length; potatis2++);
+        System.out.println("Please guess your first letter");
+        Scanner scanner2 = new Scanner(System.in);
+        String letter = scanner.next();
+        String hahakort = scanner2.toLowerCase();
+
+
+        if (letter.equals(bert[potatis2])){
+
+        }
     }
 
 }
