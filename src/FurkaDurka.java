5acddc0de38b9c24856c0ceb0734b13f3e1053bb
Lukas-Johansson <lukas.johansson4@elev.ga.ntig.se>
2022-02-08 14:35:55 +0100

    Mycket sakersamt saker

--- src/FurkaDurka.java
+++ src/FurkaDurka.java
@@ -7,27 +7,23 @@ import java.util.Scanner;
 public class FurkaDurka {
     public static void main(String[] args) {
 
+        int guessCount = 0;
+        int correctGuess = 0;
+
         System.out.println("Please type in your word");
         Scanner scanner = new Scanner(System.in);
         String word = scanner.nextLine();
         char[] ordtillsak = new char[word.length()];
-        for (int i =0; i<word.length(); i++)
-        {
+        for (int i = 0; i < word.length(); i++) {
             ordtillsak[i] = '_';
         }
+
+        System.out.println("Your word to guess is");
         System.out.println(ordtillsak);
-§
-
-        for (int potatis2 = 0; potatis2 <= bert.length; potatis2++);
         System.out.println("Please guess your first letter");
-        Scanner scanner2 = new Scanner(System.in);
-        String letter = scanner.next();
-
-
 
 
     }
-
 }
 
 
